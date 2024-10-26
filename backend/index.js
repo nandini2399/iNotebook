766e761056a0ae1f58e0030a61ae38a0afacd792
nandini2399 <nandinijairath@gmail.com>
2024-10-27 00:58:18 +0530

    router & schema setup

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,12 @@ connectToMongo();
 
 const PORT = 3000;
 
-app.get('/',(req,res)=>{
-    res.send("Hey, Nandini")
-});
+// app.get('/',(req,res)=>{
+//     res.send("Hey, Nandini")
+// });
+
+app.use('/api/auth',require('./routes/auth'));
+//app.use('/api/auth',require('./routes/notes'));
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
