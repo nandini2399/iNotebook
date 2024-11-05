13bd01bd11555531a5c13d71c55a50471d2264ef
nandini2399 <nandinijairath@gmail.com>
2024-11-05 22:11:52 +0530

    added hasing & jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const {body,validationResult} = require('express-validator')
+const bcryptjs = require('bcryptjs')
+var jwt = require('jsonwebtoken');
 
-router.get('/',[
+//create user "api/auth/createUser "
+router.post('/createUser',[
         body('name', 'Enter a valid name').isLength({ min: 3 }),
         body('email', "Enter a valid Email").isEmail(),
         body('password', 'Password must have a minimum of 5 characters').isLength({ min: 5 }),
@@ -12,19 +15,17 @@ router.get('/',[
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    // const user = User(req.body);
-    // user.save().catch(err=>{
-    //     res.json({error:err.errmsg,msg:"Duplicate email found"});
-    // });
-    //res.send(req.body)
-
     try {
+        const salt = await bcryptjs.genSalt(10);
+        const secPassword = await bcryptjs.hash(req.body.password,salt);
         const user = await User.create({
           name: req.body.name,
           email: req.body.email,
-          password: req.body.password
+          password: secPassword
         });
-        res.json(user);
+
+        var token = jwt.sign({ id: user.id }, 'shhhhh');
+        res.json(token);
       } catch (error) {
         if (error.code === 11000) {
           // Duplicate key error
